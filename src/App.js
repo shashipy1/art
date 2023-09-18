4d1d413b0702af9378fc890ca4fc981c6ad8dedb
SHASHI KANT KUMAR <shashikant.py8@gmail.com>
2023-09-19 00:34:33 +0530

    resopsive containers

--- src/App.js
+++ src/App.js
@@ -1,33 +1,60 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './Header';
-import Menu from './Menu';
-import Content from './Content';
+import Navbar from './Components/Navbar';
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
+import Contact from './Pages/Contact/Contact';
+import Footer from './Pages/Footer/Footer';
+
 
 export default function App() {
   return (
     <Router>
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          height: '100vh',
-        }}
-      >
-        <Header />
-        <div
-          style={{
-            display: 'flex',
-            flex: 1,
-            backgroundColor: 'maroon', // Set the background color for the right side
-          }}
-        >
-          <Menu />
-          <Routes>
-            <Route path="/" element={<Content />} />
-          </Routes>
-        </div>
-      </div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
     </Router>
   );
 }
+
+
+
+
+
+// import React from 'react';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import Header from './Header';
+// import Menu from './Menu';
+// import Content from './Content';
+
+// export default function App() {
+//   return (
+//     <Router>
+//       <div
+//         style={{
+//           display: 'flex',
+//           flexDirection: 'column',
+//           height: '100vh',
+//         }}
+//       >
+//         <Header />
+//         <div
+//           style={{
+//             display: 'flex',
+//             flex: 1,
+//             backgroundColor: 'maroon', // Set the background color for the right side
+//           }}
+//         >
+//           <Menu />
+//           <Routes>
+//             <Route path="/" element={<Content />} />
+//           </Routes>
+//         </div>
+//       </div>
+//     </Router>
+//   );
+// }
